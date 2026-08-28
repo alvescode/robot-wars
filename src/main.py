2fdf0abcd401867a5738d74a2a04f334cffb30bc
David Alves <davidalves.ti1@gmail.com>
2026-08-28 19:59:29 +0000

    feat: support multiple robots and empty list message

--- src/main.py
+++ src/main.py
@@ -12,17 +12,19 @@ def main():
     print("---ROBOT WARS---")
     print("Welcome to Robot Wars, a simple game based robot battle, but much fun!\n")
     print("[1] start game")
-    print("[0] end game\n")
+    print("[2] end game\n")
 
     while True:
         line = input()
+        robots = []
 
         match line.split():
             case ["1"]:
                 while True:
                     print("[1] create robot")
                     print("[2] show robot")
-                    print("[0] end game\n")
+                    print("[3] start battle")
+                    print("[4] end game\n")
 
                     line = input()
 
@@ -32,16 +34,34 @@ def main():
                             user_hp = int(input("Write robot HP: "))
                             user_attack = int(input("Write robot attack: "))
                             user_shield = int(input("Write robot shield: "))
-                            robot = Robot(user_name, user_hp, user_attack, user_shield)
+
+                            robot = Robot(
+                                user_name, 
+                                user_hp, 
+                                user_attack, 
+                                user_shield
+                            )
+
+                            robots.append(robot)
                             print("")
+                            
                         case ["2"]:
-                            print(robot)
-                        case ["0"]:
+                            if len(robots) == 0:
+                                print("Don't exist robot created!\n")
+                            else:
+                                print("---ROBOTS---")
+                                for rob in robots:
+                                    print(rob)
+
+                        case ["4"]:
                             break
+
                         case _:
                             print("Invalid command!\n")
-            case ["0"]:
+
+            case ["2"]:
                 break
+
             case _:
                 print("Invalid command!\n")
 
